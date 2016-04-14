ae737208eadfa77710dd61b91ccdb721d81e120e
palladine <duroff.83@yandex.ru>
2016-04-14 13:17:38 +0000

    v.0.01

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Lara-Test | @yield('title')</title>
+</head>
+<body>
+	
+	@include('head')
+
+	@yield('content')
+
+	@yield('footer')
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title')
+    welcome page
+@endsection
+
+@section('content')
+    <h1>The Welcome</h1>
+@endsection
